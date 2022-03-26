6210a9859222463b24c44936738a613d26d6608a
Ndeta Innocent <you@example.com>
2022-03-26 07:25:16 +0200

    pointers

--- problem1.js
+++ problem1.js
@@ -77,3 +77,35 @@ function valideAnagram(first, second){
     return true
 
 }
+
+//MULTIPLE POINTER PATTERN
+//Write a funvtion called SumZero which accepts sorted array and integers. The function should find the firsty pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if the pair does not exist
+
+//Naive solution
+function sumZero1(arr){
+    for(let i=0; i<arr.length; i++){
+        for(let j=i+1; j<arr.length; j++){
+            if(arr[i] + arr[j]===0){
+                return [arr[i], arr[j]]
+            }
+        }
+    }
+}
+
+//Refactored solution
+
+function sumZero(arr){
+    let left=0
+    let right=arr.length-1
+    while(left<right){
+        let sum=arr[left]+arr[right]
+        if(sum===0){
+            return [arr[left]+arr[right]]
+        }else if(sum>0){
+            right--
+
+        }else{
+            left++
+        }
+    }
+}
