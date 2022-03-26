d354752d716042c202eacf7cacaf67e5dd318f5b
Ndeta Innocent <you@example.com>
2022-03-26 07:46:57 +0200

    mcounts

--- problem1.js
+++ problem1.js
@@ -93,7 +93,6 @@ function sumZero1(arr){
 }
 
 //Refactored solution
-
 function sumZero(arr){
     let left=0
     let right=arr.length-1
@@ -108,4 +107,19 @@ function sumZero(arr){
             left++
         }
     }
+}
+
+//Implement a function called countUninqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted 
+function countUniqueValues(arr){
+    if(arr.length==0){
+        return 0
+    }
+    let i=0
+    for(let j=1; j<arr.length; j++){
+        if(arr[i]!==arr[j]){
+            i++
+            arr[i]=arr[j]
+        }
+    }
+    return i+1
 }
