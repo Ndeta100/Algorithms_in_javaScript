fce04151e5515b118f0d5b32da99d4161d023ef7
Ndeta Innocent <you@example.com>
2022-03-27 09:32:13 +0300

    recursion

---
+++ recursion.js
@@ -0,0 +1,30 @@
+//Define what recuursion is and how it can be used
+//Understand the two essential components of a recursive function
+//Visaulize the call stack to better debug and understand recursive functions
+//Use helper method recursion and pure recursion to solve more difficult problems 
+
+        //RECURSION IS JUST A PROCESS THAT CALLS ITSELF
+        //IN JAVASCRIPT A FUNCTION THAT CALLS ITSELF
+
+//callstack
+function takeShower(){
+    return 'Showering'
+
+}
+
+function eatBreakFast(){
+    let meal=cookFood()
+    return `Eating ${meal}`
+}
+
+function cookFood(){
+    let item=['oatmeal', 'eegs', 'protein shake']
+    return item[Math.floor(Math.random()*item.length)]
+}
+function wakeup(){
+    takeShower()
+    eatBreakFast()
+    console.log('Okay ready to go to work')
+}
+
+wakeup()
