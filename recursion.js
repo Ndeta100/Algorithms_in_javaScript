8230a685e6fbedaba27cc8c9a92315243e04b41c
Ndeta Innocent <you@example.com>
2022-03-27 22:43:55 +0300

    factorial

--- recursion.js
+++ recursion.js
@@ -44,3 +44,12 @@ function sumRange(num){
     if(num===1) return 1
     return num+sumRange(num-1)
 }
+
+//Factorial
+function factorial(num){
+    let total=1
+    for(let i=num; i>1;i--){
+        total *=i
+    }
+    return total
+}
