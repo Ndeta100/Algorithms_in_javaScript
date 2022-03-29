3213a1b79b05f73604461d7bfb920e49056dfb7b
Ndeta Innocent <you@example.com>
2022-03-29 09:24:25 +0300

    binary search

--- searchAlgorithm.js
+++ searchAlgorithm.js
@@ -13,3 +13,33 @@ function linear(arr, val){
    return -1
 }
 linear([4,5,6,3,6,2,7],7)
+
+//Binary search
+//This function accepts a sorted array and a value
+//Create a left pointer at the start of the array, and a right pointer at the end of the array
+//While the left pointer comes before the right pointer
+        //Create a pointer in the middle
+        //If you find the value you want, return the index
+        //If the value is too small, move the left pointer up
+        //If the value is too large, move the right pointer down
+//If you never find the value, return -1
+
+function binary(arr, val){
+let start=0
+let end=arr.length-1
+let middle=Math.floor((start+end)/2)
+while(arr[middle]!==val &&start<=end){
+    if(val<arr[middle]){
+        end=middle-1
+    }else{
+        start=middle+1
+    }
+    middle=Math.floor((start+end)/2)
+}
+if(arr[middle]==val){
+
+    return middle
+}else{
+    return -1
+}}
+binary([2,5,6,9,13,15,28,30],28)
