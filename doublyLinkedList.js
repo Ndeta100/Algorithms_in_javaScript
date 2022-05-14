08f47f9e310af1e949f907d71bf6e5e824bfe3c4
Ndeta Innocent <you@example.com>
2022-05-14 19:47:21 +0300

    Implementing push for doubly linked list

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -28,6 +28,15 @@ class DoublyLinkedList{
     }
     push(val){
       let newNode=new Node(val)
-      
+      if(this.length===0){
+          this.head=newNode
+          this.tail=newNode
+      }else{
+          this.tail.next=newNode
+          newNode.prev=this.tail
+          this.tail=newNode
+      }
+      this.length++
+      return this
     }
 }
