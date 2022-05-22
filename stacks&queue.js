edab8371f991d8beb836eafcaa5d3e635ecbee00
Ndeta Innocent <you@example.com>
2022-05-22 10:02:34 +0300

    Implementing queue

--- stacks&queue.js
+++ stacks&queue.js
@@ -55,3 +55,54 @@ class Stack{
        return temp.value
     }
 }
+
+//QUEUES
+let q=[]
+q.push('first')
+q.push('second')
+q.push('third')
+q.unshift()
+q.unshift()
+q.unshift()
+//ENQUEUE PSEUDOCODE
+//This function accepts some value
+//Create a new node  using that value passed to the function
+//If there are no nodes in the queue, set this node to be the first and the last property of the queue
+//Otherewise, set the next property on the current last node to be that node, and then set the last property of the queue to be that node 
+//DEQUEUE PSEUDOCODE
+//If there is no first property, just return null
+//Store the first proprty in a variable
+//See if the first is the same as the last (check if there is only 1 node). If so, set the first and the last to be null
+//If there is more than 1 node, set the first property to be the next property of the first
+//Decrement the size by 1
+//Return the value of the node dequeued 
+class Queue{
+
+    constructor(){
+        this.first=null
+        this.last=null
+        this.size=0
+    }
+    enqueue(val){
+        let newNode=new Node(val)
+        if(!this.first){
+            this.first=newNode
+            this.last=newNode
+        }else{
+        this.last.next=newNode
+        this.last=newNode
+       }
+       return this.size++ 
+    }
+    dequeue(){
+       if(!this.first) return null
+       let temp=this.first
+       if(this.first===this.last){
+           this.last=null
+       }
+       this.first=this.first.next
+       this.size--
+       return temp.value
+    }
+
+}
