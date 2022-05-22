cfe3c0621dc4fdb4fe51a26e56424829748359a4
Ndeta Innocent <you@example.com>
2022-05-22 08:37:25 +0300

    Implementing pop for a stack

--- stacks&queue.js
+++ stacks&queue.js
@@ -13,6 +13,12 @@ stack.push('discord')
 //set the next property on the node to the previously created variable
 //Increment the size of the stack by 1
 
+//POP PSEUDOCODE
+//If there are no nodes in the stack, return null
+//Create a temporary variable to store the first property on the stack
+//If there is only 1 node, set the first and last property to be null
+//If there is more than one node, set the first property to be the next property on the current first 
+//Decrement the size by 1
 class Node{
     constructor(value) {
          this.value=value
@@ -37,5 +43,15 @@ class Stack{
             this.first.next=temp
         }
         return this.size++
+    }
+    pop(){
+       if(!this.first) return null
+       let temp=this.first
+       if(this.first===this.last){
+           this.last=null
+       }
+       this.first=this.first.next
+       this.size--
+       return temp.value
     }
 }
