ffd8746345491035fbcafe268c79b0449300fa57
Ndeta Innocent <you@example.com>
2022-05-25 11:05:36 +0300

    Implementing find for a Binary search tree

--- binarySearchTrees.js
+++ binarySearchTrees.js
@@ -15,6 +15,19 @@
              //Check to see if there is a node to the left
              //If there, move to that node and repeat these steps
              //If there is not, add to that node as the left property
+    //FINDING A NODE IN A BST
+    //Starting at the root
+        //Check if there is a root, if not- we're done searching!
+        //If there is a root, check if the value of the new node is the value we are looking for. If we found it, we're are done!
+        //If not, check to see if the value is greater tahn or less tahn the value of the root
+         //If it is greater 
+            // Check to see if there is a node to the right 
+            //If there is, move to that node and repeat these steps
+            //If there is not, we're done searching
+        //If it is less
+               //Check to see if there is a node to the left 
+               //If there is, move to that node and repeat these steps
+               //If there is not, we're are done searching
     class Node{
     constructor(value){
         this.value=value
@@ -54,5 +67,20 @@ class BinarySearchTree{
        }
 
 
+    }
+    find(val){
+        if(this.root===null) return false
+        let current=this.root
+        let found=false
+        while(current && !found){
+            if(val<current.value){
+                current=current.left
+            }else if(val>current.value){
+                current=current.right
+            }else{
+                found=true
+            }
+        }
+        return
     }
 }
