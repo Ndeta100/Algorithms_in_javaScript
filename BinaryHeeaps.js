09142ee6de116014303438c4e0033f1a7d209a21
Ndeta Innocent <you@example.com>
2022-05-28 11:37:13 +0300

    Implementing priority queue  for  Heap

--- BinaryHeeaps.js
+++ BinaryHeeaps.js
@@ -57,7 +57,7 @@ class MaxBinaryHeap{
   sinkDown(){
       let index=0
       const length=this.values.length
-      const element=this.values[0]
+      const element=this.values[index]
       while(true){
           let leftChildIndex=2*index+1
           let rightChildIndex=2*index+2
@@ -87,3 +87,78 @@ class MaxBinaryHeap{
       }
   }
 }
+//PRIORITY QUEUE
+//Write  a Min Binary heap- lower number means higher priority
+//Each node has a val and a priority. Use the priority to build the heap
+//Enqueue method accepts a value and a priority, makes a new node, put it in the right spot based off of it's priority
+//Dequeue method removes root element, returns it, and rearrange heap using priority
+class Node{
+    constructor(val, priority){
+        this.values=val
+        this.priority=priority
+
+    }
+}
+class PriorityQueue{
+    constructor(){
+        this.values=[]
+    }
+    enqueue(val, priority){
+        let newNode=new Node(val, priority)
+        this.values.push(newNode)
+        this.bubbleUp()
+    }
+    bubbleUp(){
+        let index=this.values.length-1
+        const element=this.values[index]
+        while(index>0){
+        let parentIndex=Math.floor((index-1)/2)
+        let parent=this.values[parentIndex]
+        if(element.priority<=parent.priority) break
+        this.values[parentIndex]=element
+        this.values[index]=parent
+        index=parentIndex
+        }
+    }
+     dequeue(){
+         const  max=this.values[0]
+         const end=this.values.pop()
+         if(this.values.length>0){
+             this.values[0]=end
+             this.sinkDown()
+         }
+         return max
+     }
+     sinkDown(){
+         let index=0
+         const length=this.values.length
+         const element=this.values[index]
+         while(true){
+             let leftChildIndex=2*index+1
+             let rightChildIndex=2*index+2
+             let leftChild, rightChild
+             let swap=null
+             if(leftChildIndex<length){
+                 leftChild=this.values[leftChildIndex]
+                 if(leftChild.priority>element.priority){
+                     swap=leftChildIndex
+                 }
+             }
+             if(rightChildIndex<length){
+                 rightChild=this.values[rightChildIndex]
+                 if( 
+                     
+                     (swap===null && rightChild.priority>element.priority) ||
+                     (swap!==null && rightChild.priority>leftChild.priority)
+                   
+                    ){
+                     swap=rightChildIndex
+                 }
+             }
+             if(swap==null)break
+             this.values[index]=this.values[swap]
+             this.values[swap]=element
+             index=swap
+         }
+     }
+   }
