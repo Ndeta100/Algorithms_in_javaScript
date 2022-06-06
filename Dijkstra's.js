d596cedea278e63668f6ddde31481eaa8f6e068c
Ndeta Innocent <you@example.com>
2022-06-06 07:04:56 +0300

    Implementing  dijkstra's algorithm

--- Dijkstra's.js
+++ Dijkstra's.js
@@ -28,3 +28,67 @@ class PriorityQueue{
         this.values.sort((a,b)=>a.priority-b.priority)
     }
 }
+
+class WeightedGraph{
+    constructor(){
+        this.adjacencyList={}
+    }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex]=[]
+    }
+    addEdge(vertex1, vertex2, weight){
+        this.adjacencyList[vertex1].push({node:vertex2, weight})
+        this.adjacencyList[vertex2].push({node:vertex1, weight})
+    }
+
+    Dijkstra(start, finish){
+       const nodes=new PriorityQueue()
+       const distances={}
+       const previous={}
+       let path=[] //to return at end
+       let smallest
+         //Build up initial state
+         for(let vertex in this.adjacencyList){
+             if(vertex===start){
+                 distances[vertex]=0
+                 nodes.enqueue(vertex, 0)
+             }else{
+                 distances[vertex]=Infinity
+                 nodes.enqueue(vertex, Infinity)
+             }
+             previous[vertex]=null
+         }
+         //As long as there is something to visit
+         while(node.values.length){
+             smallest=node.enqueue().val
+             if(smallest===finish){
+                 //We are done
+                 //Build up path to return at end
+                 while(previous[smallest]){
+                     path.push(smallest)
+                     smallest=previous[smallest]
+                 }
+                 break
+             }
+             if(smallest || distances[smallest] !==Infinity){
+                 for(let neighbor in this.adjacencyList[smallest]){
+                     //Find neighboring node
+                     let nextNode=this.adjacencyList[smallest][neighbor]
+                     console.log(nextNode)
+                     //calculate new distance to neighboring node
+                     let candidate=distance[smallest]+ nextNode.weight
+                     let nextNeighbor=nextNode.weight
+                     if(candidate<distances[nextNeighbor]){
+                         //update new smallest distance to neighbor
+                         distances[nextNeighbor]=candidate
+                         //updating previous How we got to neighbor
+                         previous[nextNeighbor]=smallest
+                         //enqueue in priority queue with new priority
+                         nodes.enqueue(nextNeighbor, candidate)
+                     }
+                 }
+             }
+         }
+         return path.concat(smallest.revers())
+    }
+}
